bc460a6104ad78f106fdfd8cfed1cd86fec65e67
Ethan721au <stephane.mercier.au@gmail.com>
2024-10-17 19:50:05 +1100

    chnages

--- src/components/TicTacToe/Square/Square.tsx
+++ src/components/TicTacToe/Square/Square.tsx
@@ -5,15 +5,30 @@ import styles from "./square.module.css";
 export default function Square({
   value,
   action,
-  inputKey,
+  colIndex,
+  rowIndex,
+  player,
 }: {
   value: number;
-  action: () => void;
-  inputKey: number;
+  action: (newInput: number, rowIndex: number, colIndex: number) => void;
+  colIndex: number;
+  rowIndex: number;
+  player: string;
 }) {
+  const updateInput = () => {
+    if (value !== 0) return;
+    if (player === "Circle") {
+      action(-1, rowIndex, colIndex);
+    } else {
+      action(1, rowIndex, colIndex);
+    }
+  };
+
+  const displayValue = value === 1 ? "X" : value === -1 ? "O" : "";
+
   return (
-    <button className={styles.container} onClick={action} key={inputKey}>
-      {value}
+    <button className={styles.container} onClick={updateInput} key={""}>
+      {displayValue}
     </button>
   );
 }
--- src/components/TicTacToe/TicTacToe.tsx
+++ src/components/TicTacToe/TicTacToe.tsx
@@ -1,52 +1,109 @@
 "use client";
 
-import { useRef, useState } from "react";
+import { useState } from "react";
 import Square from "./Square/Square";
+import styles from "./ticTacToe.module.css";
 
 export default function TicTacToe() {
-  const row = 3;
-  const col = 3;
+  const defaultMatrix = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+  ];
   const [player, setPlayer] = useState("Circle");
-  const square = useRef();
-  console.log(square);
+  const [matrix, setMatrix] = useState(defaultMatrix);
+  // console.log(matrix);
 
-  const recordInput = () => {
-    console.log(colIndex);
-    if (player === "Circle") {
-      setPlayer("Cross");
-    } else {
-      setPlayer("Circle");
-    }
+  const recordInput = (
+    newInput: number,
+    rowIndex: number,
+    colIndex: number
+  ) => {
+    setMatrix((oldMatrix) => {
+      const newMatrix = [...oldMatrix];
+      newMatrix[rowIndex][colIndex] = newInput;
+      determineWinner(newMatrix);
+      return newMatrix;
+    });
 
-    return 5;
+    setPlayer(player === "Circle" ? "Cross" : "Circle");
   };
 
-  const generateMatrix = (rows: number, cols: number) => {
-    const matrix = [];
-    for (let i = 0; i < rows; i++) {
-      matrix.push(Array(cols).fill(5));
-    }
-    return matrix;
-  };
+  const determineWinner = (matrix: number[][]) => {
+    console.log(matrix, "matrix");
 
-  const matrix = generateMatrix(row, col);
+    //horizontal
+    const horizontal = matrix.map((row) => row.reduce((a, b) => a + b, 0));
+    console.log(horizontal, "horizontal");
+    for (let i = 0; i < horizontal.length; i++) {
+      console.log(horizontal[i], "horizontal[i]");
+      if (horizontal[i] === 3) {
+        console.log("Cross wins");
+      }
+      if (horizontal[i] === -3) {
+        console.log("Circle wins");
+      }
+    }
+
+    // const sum = numbers.reduce((accumulator, currentValue) => {
+    //   return accumulator + currentValue;
+    // }, 0);
+
+    // const winningCombos = [
+    //   // horizontal
+    //   [0, 1, 2],
+    //   [3, 4, 5],
+    //   [6, 7, 8],
+    //   // vertical
+    //   [0, 3, 6],
+    //   [1, 4, 7],
+    //   [2, 5, 8],
+    //   // diagonal
+    //   [0, 4, 8],
+    //   [2, 4, 6],
+    // ];
+
+    // for (let i = 0; i < winningCombos.length; i++) {
+    //   const [a, b, c] = winningCombos[i];
+    //   console.log(a);
+    //   if (
+    //     matrix[a] &&
+    //     matrix[a] === matrix[b] &&
+    //     matrix[a] === matrix[c] &&
+    //     matrix[a] !== 0
+    //   ) {
+    //     console.log("winner");
+    //     return matrix[a];
+    //   }
+    // }
+  };
 
   return (
     <>
       <div>{player}</div>
       {matrix &&
         matrix.map((row, rowIndex) => (
-          <div key={rowIndex}>
+          <div key={rowIndex} className={styles.container}>
             {row.map((col, colIndex) => (
               <Square
                 key={colIndex}
-                inputKey={colIndex}
-                value={rowIndex + colIndex}
+                colIndex={colIndex}
+                rowIndex={rowIndex}
+                value={matrix[rowIndex][colIndex]}
                 action={recordInput}
+                player={player}
               />
             ))}
           </div>
         ))}
+      <button
+        onClick={() => {
+          setMatrix(defaultMatrix);
+          setPlayer("Circle");
+        }}
+      >
+        reset
+      </button>
     </>
   );
 }
