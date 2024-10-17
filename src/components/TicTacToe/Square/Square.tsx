124df2321282c619d1c0edcda32746b45cd1a551
Ethan721au <stephane.mercier.au@gmail.com>
2024-10-17 15:54:21 +1100

    changes

--- src/components/TicTacToe/Square/Square.tsx
+++ src/components/TicTacToe/Square/Square.tsx
@@ -1,3 +1,19 @@
-export default function Square({ value }: { value: number }) {
-  return <button className="square">{value}</button>;
+"use client";
+
+import styles from "./square.module.css";
+
+export default function Square({
+  value,
+  action,
+  inputKey,
+}: {
+  value: number;
+  action: () => void;
+  inputKey: number;
+}) {
+  return (
+    <button className={styles.container} onClick={action} key={inputKey}>
+      {value}
+    </button>
+  );
 }
--- src/components/TicTacToe/TicTacToe.tsx
+++ src/components/TicTacToe/TicTacToe.tsx
@@ -1,28 +1,49 @@
-import React from "react";
+"use client";
+
+import { useRef, useState } from "react";
 import Square from "./Square/Square";
 
-const generateMatrix = (rows: number, cols: number) => {
-  const matrix = [];
-  for (let i = 0; i < rows; i++) {
-    matrix.push(Array(cols).fill(5));
-  }
-  console.log(matrix);
-  return matrix;
-};
-
-const generateDisplay = [5, 6, 7];
-
 export default function TicTacToe() {
-  const matrix = generateMatrix(3, 3);
-  console.log(matrix);
+  const row = 3;
+  const col = 3;
+  const [player, setPlayer] = useState("Circle");
+  const square = useRef();
+  console.log(square);
+
+  const recordInput = () => {
+    console.log(colIndex);
+    if (player === "Circle") {
+      setPlayer("Cross");
+    } else {
+      setPlayer("Circle");
+    }
+
+    return 5;
+  };
+
+  const generateMatrix = (rows: number, cols: number) => {
+    const matrix = [];
+    for (let i = 0; i < rows; i++) {
+      matrix.push(Array(cols).fill(5));
+    }
+    return matrix;
+  };
+
+  const matrix = generateMatrix(row, col);
 
   return (
     <>
+      <div>{player}</div>
       {matrix &&
         matrix.map((row, rowIndex) => (
           <div key={rowIndex}>
             {row.map((col, colIndex) => (
-              <Square key={colIndex} value={col} />
+              <Square
+                key={colIndex}
+                inputKey={colIndex}
+                value={rowIndex + colIndex}
+                action={recordInput}
+              />
             ))}
           </div>
         ))}
